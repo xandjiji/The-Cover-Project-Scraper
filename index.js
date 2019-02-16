5ad94a318b0bb4bba2613e5e46b8ef91bbfe091b
xand <xandjiji@gmail.com>
2019-02-15 23:04:21 -0200

    added game console to the filename

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ console.log("bot iniciado!");
 
 function vai(id){
 
+     var timestamp = new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"});
+
      rp(url + id)
           .then(function(html){
 
@@ -31,7 +33,11 @@ function vai(id){
                var link = baseURL + $('h2 > a', html).attr('href');
 
                // parser pra pegar o titulo do jogo
-               //var title = $('title', html).text();
+               var gameConsole = $('title', html).text();
+
+               var index = gameConsole.indexOf('(');
+               gameConsole = gameConsole.substring(index, gameConsole.length - 19);
+
                var title = link;
                title = title.substring(55, title.length);
 
@@ -44,15 +50,15 @@ function vai(id){
 
                          sharp("cover.jpg")
                               .jpeg( { quality: 50 } )
-                              .toFile('saved/' + title)
+                              .toFile('saved/' + gameConsole + title)
                               .then(info => {
-                                   console.log(title + ' finished (#' + id + ')');
+                                   console.log('[' + timestamp + '] ' + gameConsole + title + ' finished (#' + id + ')');
                                    if(id != 16706){
                                         vai(id+1);
                                    }
                               })
                               .catch(err => {
-                                   console.log("ERROR: #" + id + " failed.");
+                                   console.log('[' + timestamp + '] ' + 'ERROR: #' + id + ' failed.');
                                    if(id != 16706){
                                         vai(id+1);
                                    }
@@ -60,7 +66,7 @@ function vai(id){
                     }));
                })
                .on("error", (err) => {
-                    console.log("ERROR: #" + id + " failed.");
+                    console.log('[' + timestamp + '] ' + 'ERROR: #' + id + ' failed.');
                     if(id != 16706){
                          vai(id+1);
                     }
